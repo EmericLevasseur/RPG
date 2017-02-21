de3a6c71c234b6fc09e9b43132d420e1980b8e14
Corentin Leroy <leroy.corentin@outlook.fr>
2017-02-21 12:46:41 +0100

    cocomit4

--- inscription.php
+++ inscription.php
@@ -1,33 +1,48 @@
-<form method="post" action="inscription.traitement.php">
+<?php include 'inscription.traitement.php'; ?>
 
-   <fieldset>
-       <legend>Inscription</legend> <!-- Titre du fieldset -->
-<p>
-       <label for="nom">Quel est votre nom ?</label>
-       <input type="text" name="nom" id="nom" />
-</p>
-<p>
-       <label for="prenom">Quel est votre prénom ?</label>
-       <input type="text" name="prenom" id="prenom" />
-</p>
-</p>
-       <label for="prenom">Quel est votre pseudo ?</label>
-       <input type="text" name="pseudo" id="pseudo" />
-<p>
-</p>
-       <label for="email">Quel est votre e-mail ?</label>
-       <input type="email" name="email" id="email" />
-<p>
-       <label for="prenom">Votre descriptif ?</label>
-       <input type="text" name="descriptif" id="descriptif" />
-</p>
-<p>
-      <label for="mdp">Mot de passe</label><br />
-      <input type="password" name="mdp" id="mdp" size="30" />
-</p>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <form method="post" action="inscription.php">
 
-<br />
+       <fieldset>
+           <legend>Inscription</legend> <!-- Titre du fieldset -->
+    <p>
+           <label for="nom">Quel est votre nom ?</label>
+           <input type="text" name="nom" id="nom" />
+    </p>
+    <p>
+           <label for="prenom">Quel est votre prénom ?</label>
+           <input type="text" name="prenom" id="prenom" />
+    </p>
+    </p>
+           <label for="prenom">Quel est votre pseudo ?</label>
+           <input type="text" name="pseudo" id="pseudo" />
+    <p>
+    </p>
+           <label for="email">Quel est votre e-mail ?</label>
+           <input type="email" name="email" id="email" />
+    <p>
+           <label for="prenom">Votre descriptif ?</label>
+           <input type="text" name="descriptif" id="descriptif" />
+    </p>
+    <p>
+          <label for="mdp">Mot de passe</label><br />
+          <input type="password" name="mdp" id="mdp" size="30" />
+    </p>
 
-      <input type="submit" value="Valider" class="valider" />
-   </fieldset>
-</form>
+    <br />
+
+          <input type="submit" value="Valider" class="valider" />
+       </fieldset>
+    </form>
+  </body>
+</html>
--- inscription.traitement.php
+++ inscription.traitement.php
@@ -1,57 +1,43 @@
 <?php
-    if(isset($_POST['nom']) AND
-    isset($_POST['prenom']) AND
-    isset($_POST['pseudo']) AND
-    isset($_POST['email']) AND
-    isset($_POST['descriptif']) AND
-    isset($_POST['mdp']))
-    {
-        try
-        {
-            $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-            $bdd = new PDO('mysql:host=localhost;dbname=phppoo', 'root', 'root',   $pdo_options);
 
-            $req = $bdd->prepare('INSERT INTO Joueur(nom ,prenom, pseudo, email, descriptif, mdp) VALUES(:nom, :prenom, :pseudo, :email, :descriptif, :mdp)');
-            $req->execute(array(
-            'nom' => $_POST['nom'],
-            'prenom' => $_POST['prenom'],
-            'pseudo' => $_POST['pseudo'],
-            'email' => $_POST['email'],
-            'descriptif' => $_POST['descriptif'],
-            'mdp' => $_POST['mdp']
-            ));
+require_once ('config.php');
 
-            header('Location: inscription.php');
-        }
-        catch(Exception $e)
-        {
-            die('Erreur : '.$e->getMessage());
-        }
+    if(isset($_POST['nom']) AND isset($_POST['prenom']) AND isset($_POST['pseudo']) AND isset($_POST['email']) AND isset($_POST['descriptif']) AND isset($_POST['mdp']))
+    {
+      if($_POST['nom'] == NULL)
+      {
+          echo 'Vous devez entrer un nom';
+      } elseif($_POST['prenom'] == NULL)
+      {
+          echo 'Vous devez entrer un prenom';
+      } elseif($_POST['pseudo'] == NULL)
+      {
+          echo 'Vous devez confirmer votre pseudo';
+      } elseif($_POST['email'] == NULL)
+      {
+          echo 'Vous devez crééer votre mail';
+      } elseif($_POST['descriptif'] == NULL)
+      {
+          echo 'Vous devez insérer un descriptif';
+      } elseif($_POST['mdp'] == NULL)
+      {
+          echo 'Vous devez insérer un mot de passe';
+      } else {
+        $req = $bdd->prepare('INSERT INTO Joueur(nom ,prenom, pseudo, email, descriptif, mdp) VALUES(:nom, :prenom, :pseudo, :email, :descriptif, :mdp)');
+        $req->execute(array(
+        'nom' => $_POST['nom'],
+        'prenom' => $_POST['prenom'],
+        'pseudo' => $_POST['pseudo'],
+        'email' => $_POST['email'],
+        'descriptif' => $_POST['descriptif'],
+        'mdp' => md5($_POST['mdp'])
+        ));
+
+        header('Location: login.php');
+      }
+
+
     }
 
-    if($_POST['nom'] == NULL)
-    {
-        echo 'Vous devez entrer un nom';
-    }
-    if($_POST['prenom'] == NULL)
-    {
-        echo 'Vous devez entrer un prenom';
-    }
-    if($_POST['pseudo'] == NULL)
-    {
-        echo 'Vous devez confirmer votre pseudo';
-    }
-    if($_POST['email'] == NULL)
-    {
-        echo 'Vous devez crééer votre mail';
-    }
-    if($_POST['descriptif'] == NULL)
-    {
-        echo 'Vous devez insérer un descriptif';
-    }
-    if($_POST['mdp'] == NULL)
-    {
-        echo 'Vous devez insérer un mot de passe';
-    }
-    header('Location: inscription.php');
+
 ?>
